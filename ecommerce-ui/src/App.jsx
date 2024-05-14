8e09f3f2a4ff376eecdfb6b04bccc7564b77d625
alexakerr <155467834+alexakerr@users.noreply.github.com>
2024-05-13 22:37:12 -0400

    Update App.jsx

--- ecommerce-ui/src/App.jsx
+++ ecommerce-ui/src/App.jsx
@@ -1,23 +1,22 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-// internal imports
-import './App.css'; //keep as CSS stylesheet so global CSS styling
-import CustomerList from './components/CustomerList';
-import CustomerForm from './components/CustomerForm';
-import Home from './components/Home';
-import NotFound from './components/NotFound';
-// import OrderList from './components/OrderList';
+
+import './App.css'; 
+import MoviesList from './components/moviesList';
+import UserProfile from './components/userProfile';
+
+
 
 function App(){
   return (
     <div className="app-container">
       <Routes>
-        {/* all different pages now instead of inside a component */}
+        {}
         <Route path='/' element={<Home />} />
-        <Route path='/customers' element={<CustomerList /> } /> 
-        <Route path='/add-customer' element={<CustomerForm />} />
-        <Route path='/edit-customer/:id' element={<CustomerForm />} />
-        {/* catch all the other paths not associated with a page */}
+        <Route path='/movies' element={<MoviesListList /> } /> 
+        <Route path='/add-movie' element={<MoviesList />} />
+        <Route path='/edit-movie/:id' element={<MoviesList />} />
+        {}
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
@@ -27,30 +26,3 @@ function App(){
 
 export default App
 
-
-// OLD App component
-// function App(props) {
-//   // initializing state with hooks for functional components
-//   const [selectedCustomerId, setCustomerId] = useState(null)
-  
-//   // you don't always need to mount your component if you don't need your component to 
-//   // do anything when it initially renders
-  
-//   // need to be setup this way when we pass a function/event into a child component
-//   const handleCustomerSelect = (customerId) => {
-//     setCustomerId(customerId)
-//   }
-
-//   return (
-//     <div className='app-container'>
-//       <h1> {props.title} </h1>
-//       <CustomerList onCustomerSelect= { handleCustomerSelect } />
-//       { selectedCustomerId && 
-//         <div>
-//           <h2>Selected Customer: {selectedCustomerId}</h2> 
-//           <OrderList customerId={selectedCustomerId} />
-//         </div>
-//       }
-//     </div>
-//   )
-// }
